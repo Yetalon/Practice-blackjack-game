130b9bff57cbc9b101b8211f313f4815ea39cd83
Yetalon <Davisac7@etsu.edu>
2023-10-30 20:02:49 -0400

    Refactoring

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let sums = {
     sum: 0,
     dealers_sum: 0,
 };
+
 let drawbutton = document.getElementById("draw-btn");
 drawbutton.style.display = "none";
 let standbutton = document.getElementById("stand-btn");
@@ -16,25 +17,22 @@ standbutton.style.display = "none";
 let newgamebutton = document.getElementById("newgame-btn");
 newgamebutton.style.display = "none";
 let startgamebutton = document.getElementById("startgame-btn");
+startgamebutton.style.display = "inline";
+
 function start (){
-    newgamebutton.style.display = "none";
     let { playersum, dealers_sum } = startingcards();
     game(playersum, dealers_sum);
     if (playersum === 21) {
         message = "Blackjack!";
         hasBlackJack = true;
         messageEL.textContent = message;  
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
+        cleardisplay()
     }
     else if (playersum > 21) {
         message = "Bust!";
         isAlive = false;
         messageEL.textContent = message;  
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
+        cleardisplay()
     }
         
     startgamebutton.style.display = "none";
@@ -59,8 +57,6 @@ function getRandomCard(){
     return Math.floor(Math.random() * 11)+1; 
 }
 
-let hasBlackJack = false;
-let isAlive = true;
 let message;
 let messageEL = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -68,30 +64,22 @@ let CardsEl = document.getElementById("cards-el");
 
 function game(sum, dealers_sum) {
     CardsEl.textContent = "Cards: " + cards[0] + " , " +  cards[1];
-    for(let i=0; i<cards.length; i++){
-        if(cards[i] !== undefined && cards[i] != cards[0] && cards[i] != cards[1]){
+    for(let i=2; i<cards.length; i++){
             CardsEl.textContent += " , " + cards[i];
-        }
     }
     dealerscardsEL.textContent = "Dealer Cards: " + dealerscards[0];
     dealerssumEL.textContent = "Dealer Total: " + dealers_sum;
     sumEl.textContent = "Total: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw?";
-    } else if (sum === 21) {
+    } 
+    else if (sum === 21) {
         message = "Blackjack!";
-        hasBlackJack = true;
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
-        
-    } else {
+        cleardisplay()
+    } 
+    else {
         message = "Bust!";
-        isAlive = false;
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
-
+        cleardisplay()
     }
     messageEL.textContent = message;    
 };
@@ -101,9 +89,7 @@ let newcard;
 function draw(){
     let sum = 0;
     for(let i=0; i<cards.length; i++){
-        if(cards[i] !== undefined){
             sum += cards[i];
-        }
     }
     let dealers_sum = dealerscards[0];
     newcard = getRandomCard();
@@ -114,10 +100,8 @@ function draw(){
 
 function stand() {
     let dealers_sum = 0;
-    for (let j = 0; j < dealerscards.length; j++) {
-        if (dealerscards[j] !== undefined) {
-            dealers_sum += dealerscards[j];
-        }
+    for (let i = 0; i < dealerscards.length; i++) {
+            dealers_sum += dealerscards[i];
     }
     while (dealers_sum <= 16) {
         let newcard = getRandomCard();
@@ -126,44 +110,35 @@ function stand() {
     }
     let sum = 0;
     for(let i=0; i<cards.length; i++){
-        if(cards[i] !== undefined){
             sum += cards[i];
-        }
     }
     if (dealers_sum > 21) {
         message = "Dealer Bust! You Win!";
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
+        cleardisplay()
     }
     else if (dealers_sum < sum && sum <= 21){
         message = "You Win!";
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
-    }
-    else if (sum > 21){
-        message = "You Bust! Dealer Wins!";
+        cleardisplay()
     }
     else if (dealers_sum === sum){
         message = "Draw!";
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
+        cleardisplay()
     }
     else if (dealers_sum > sum){
         message = "Dealer Wins!";
-        drawbutton.style.display = "none";
-        standbutton.style.display = "none";
-        newgamebutton.style.display = "inline";
+        cleardisplay()
     }
-    for (let j = 0; j < dealerscards.length; j++) {
-        if (dealerscards[j] !== undefined && dealerscards[j] !== dealerscards[0]) {
-            dealerscardsEL.textContent += " , " + dealerscards[j];
-        }
+    for (let i = 1; i < dealerscards.length; i++) {
+            dealerscardsEL.textContent += " , " + dealerscards[i];
     }
     dealerssumEL.textContent = "Total: " + dealers_sum;
     messageEL.textContent = message;
+}
+
+function cleardisplay(){
+    drawbutton.style.display = "none";
+    standbutton.style.display = "none";
+    newgamebutton.style.display = "inline";
 }
 
 function startover(){
